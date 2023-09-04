4c2aa3bedc976a7b3791f25986099c64695a41c7
k.otoro <k.otoro@gemspaysolution.com>
2023-09-04 19:08:22 +0100

    quick fixing

--- app/lib/shareds/resources/routes/app_pages.dart
+++ app/lib/shareds/resources/routes/app_pages.dart
@@ -1,15 +1,14 @@
 import 'package:get/get.dart';
 
 import '../../../controllers/bindings/dashboard_tab_binding.dart';
-import '../../../screens/auth/account_type_screen.dart';
+import '../../../screens/onboarding/onboarding_screen.dart';
 import 'app_reoutes.dart';
 
 class AppPages {
   static var list = [
     GetPage(
       name: AppRoutes.onboarding,
-      page: () => const AccountTypeScreen(),
-      // page: () => OnBoardingScreen(),
+      page: () => OnBoardingScreen(),
       // page: () => HomeLandingTabScreen(),
       binding: DashboardTabBinding(),
     ),
