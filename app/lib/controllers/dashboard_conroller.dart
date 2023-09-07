06713d06bb0340713cc8b9e2ac9bd9f561ea5533
noahweasley <iebenmelu@gmail.com>
2023-09-07 03:16:22 +0100

    Add custom app dialog

--- app/lib/controllers/dashboard_conroller.dart
+++ app/lib/controllers/dashboard_conroller.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: avoid_print, unnecessary_string_interpolations
 import 'package:app/controllers/bloc/user_controller.dart';
 import 'package:app/widgets/currency_format.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../shareds/managers/get_session_manager.dart';
+import '../widgets/app_dialog.dart';
+import '../widgets/app_styles.dart';
 
 class DashboardController extends GetxController {
   final showBalance = true.obs;
@@ -45,5 +48,13 @@ class DashboardController extends GetxController {
       accountNumber.value = response.data!.accountNumber;
       balance.value = formatCurrency(response.data!.balance);
     }
+  }
+
+  Future<dynamic> displayPlaceholderDialog(String pageTitle) {
+    return showAppDialog(
+      title: pageTitle,
+      subtitle: 'We have you in mind. In the main time, explore the available features. Thank you.',
+      type: DialogType.warning,
+    );
   }
 }
