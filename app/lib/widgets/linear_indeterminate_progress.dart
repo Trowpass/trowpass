5b5fe50db6e8283066790f9a2ce4a32653875d42
noahweasley <iebenmelu@gmail.com>
2023-09-09 00:37:28 +0100

    Initialize remake indeterminate progress task

---
+++ app/lib/widgets/linear_indeterminate_progress.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LinearIndeterminateProgress extends StatelessWidget {
+  final bool isLoaded;
+
+  const LinearIndeterminateProgress({super.key, required this.isLoaded});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center();
+  }
+}
