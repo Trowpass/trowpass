92c81781fcc60825576fb7d9c29e7c5a08f04395
noahweasley <iebenmelu@gmail.com>
2023-10-24 18:26:54 +0100

    Fix transaction list item click and appearance

--- app/lib/widgets/history_list_item.dart
+++ app/lib/widgets/history_list_item.dart
@@ -6,70 +6,69 @@ import '../../shareds/utils/app_colors.dart';
 import '../../shareds/utils/border_radius.dart';
 import '../../widgets/app_styles.dart';
 
-class HistoryListItem extends StatefulWidget {
+class HistoryListItem extends StatelessWidget {
   final TransactionHistoryData history;
 
   const HistoryListItem(this.history, {super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    return HistoryListItemState();
-  }
-}
-
-class HistoryListItemState extends State<HistoryListItem> {
-  @override
   Widget build(BuildContext context) {
-    var transactionType = widget.history.paymentType;
+    var transactionType = history.paymentType;
     var color = transactionType == PaymentType.payIn ? successColor : validationErrorColor;
 
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: GestureDetector(
-        onTap: () {},
-        child: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: background,
-            border: Border.all(color: greyLight, width: 0.3),
+    return Column(
+      children: [
+        Material(
+          borderRadius: BorderRadius.circular(cardBorderRadius),
+          child: InkWell(
             borderRadius: BorderRadius.circular(cardBorderRadius),
-          ),
-          padding: const EdgeInsets.all(14),
-          child: Row(
-            children: <Widget>[
-              _buildIndicator(transactionType),
-              Padding(
-                padding: const EdgeInsets.only(left: 18),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      widget.history.createdBy,
-                      style: appStyles(14, titleActive, FontWeight.bold),
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      widget.history.formattedDate,
-                      style: appStyles(12, gray, FontWeight.normal),
-                    ),
-                  ],
-                ),
+            onTap: () {},
+            child: Ink(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: background,
+                border: Border.all(color: border, width: 0.3),
+                borderRadius: BorderRadius.circular(cardBorderRadius),
               ),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Text(
-                      widget.history.displayAmount,
-                      style: appStyles(14, color, FontWeight.w400),
+              padding: const EdgeInsets.all(14),
+              child: Row(
+                children: <Widget>[
+                  _buildIndicator(transactionType),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 18),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          history.createdBy,
+                          style: appStyles(14, titleActive, FontWeight.bold),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          history.formattedDate,
+                          style: appStyles(12, gray, FontWeight.normal),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              )
-            ],
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Text(
+                          history.displayAmount,
+                          style: appStyles(14, color, FontWeight.w400),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
-      ),
+        const SizedBox(height: 16),
+      ],
     );
   }
 
